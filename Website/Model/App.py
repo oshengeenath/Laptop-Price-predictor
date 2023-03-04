26fac92121b2cda34440d1759232154c42d0cbb5
osheng7 <oshengeenath2002@gmail.com>
2023-03-04 14:08:36 +0530

    flask

--- Website/Model/App.py
+++ Website/Model/App.py
@@ -18,7 +18,18 @@ def index():
         ips = request.form.getlist("ips")
 
         feature_list = []
-        feature_list.append(int())
+        feature_list.append(int('Ram'))
+        feature_list.append(float('Weight'))
+        feature_list.append(len('Touchscreen'))
+        feature_list.append(len('IPS'))
+
+        company_list = ['Company_Acer', 'Company_Apple','Company_Asus', 'Company_Dell', 'Company_HP', 'Company_Lenovo','Company_MSI', 'Company_Other', 'Company_Toshiba']
+        typename_list = ['TypeName_2 in 1 Convertible', 'TypeName_Gaming', 'TypeName_Netbook','TypeName_Notebook', 'TypeName_Ultrabook', 'TypeName_Workstation']
+        opsys_list = ['OpSys_Linux', 'OpSys_Mac', 'OpSys_Others', 'OpSys_Windows']
+        cpu_list = ['CPU_name_AMD', 'CPU_name_Intel Core i3', 'CPU_name_Intel Core i5','CPU_name_Intel Core i7', 'CPU_name_Others']
+        gpu_list = ['GPU_name_AMD','GPU_name_Intel', 'GPU_name_Nvidia']
+
+
 
     return render_template('index.HTML')
 

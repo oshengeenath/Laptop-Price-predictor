4b76f9d69fe3f885d86c7f0ec91f3688595995ec
osheng7 <oshengeenath2002@gmail.com>
2023-03-04 12:03:32 +0530

    flask

--- Website/Model/App.py
+++ Website/Model/App.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+import requests
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=["POST","GET"])
 def index():
+    if request.method == "POST":
+        ram = request.form["ram"]
+        weight = request.form["weight"]
+        company = request.form["company"]
+        typename = request.form["typename"]
+        opsys = request.form["opsys"]
+        cpu = request.form["cpuname"]
+        gpu = request.form["gpuname"]
+        touchscreen = request.form.getlist("touchscreen")
+        ips = request.form.getlist("ips")
+
     return render_template('index.HTML')
 
 if __name__ == '__main__':
